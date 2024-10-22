a8f6f5281fe3b386b775f82934df514b067d77b9
khalid-sr-hasan <khalid.sr.hasan@gmail.com>
2024-10-22 12:36:20 +0600

    create solo-sphere-server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("soloSphere server is running");
+});
+
+app.listen(port, () => {
+    console.log("server is running PORT:", port);
+});
